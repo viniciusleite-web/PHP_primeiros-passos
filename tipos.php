f70eea733884f90e79a8bb6bad585b3c272b6dd6
Vinicius leite <viniciusleite094@gmail.com>
2023-03-24 22:37:13 -0300

    Tipos de dados
    Tipos primitivos

---
+++ tipos.php
@@ -0,0 +1,19 @@
+<?php
+
+$idade = 21;
+// integer
+
+$salario = 1000.301;
+// double ou float
+
+$divisao = 10 / 3;
+
+$texto = "Olá mundo";
+// string
+
+$verdadeiro = true;
+$falso = false;
+// boolean
+
+// TIPOS PRIMITIVOS
+echo gettype($falso);
