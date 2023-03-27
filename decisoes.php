89e7cfee0ad990d16ed0f36d5b7b8757f4d583b7
Vinicius leite <viniciusleite094@gmail.com>
2023-03-27 18:57:41 -0300

    Senão

--- decisoes.php
+++ decisoes.php
@@ -1,12 +1,19 @@
 <?php
 
-$idade = 16;
+$idade = 17;
 
 
 $nome = 'Vinicius';
+$numeroDePessoas  = 1;
 
-echo "Você só pode entrar se tiver a partir de 18 anos." . PHP_EOL;
-if($idade >= 18 and $nome == 'Vinicius'){
+echo "Você só pode entrar se tiver a partir de 18 anos ou";
+ echo "a partir de 16 anos se tiver acompanhado." . PHP_EOL;
+if($idade >= 18){
     echo "Você tem $idade anos" . PHP_EOL;
     echo 'Pode entrar';
+}else{
+    echo "Você só tem $idade anos . Você não pode entrar";
 }
+
+echo PHP_EOL;
+echo "Adeus!";
