751dc259b25f6c39d27cb8305a097aba93aaea73
Vinicius leite <viniciusleite094@gmail.com>
2023-03-27 20:48:47 -0300

    Repeticao

--- decisoes.php
+++ decisoes.php
@@ -1,6 +1,6 @@
 <?php
 
-$idade = 17;
+$idade = 18;
 
 
 $nome = 'Vinicius';
@@ -10,13 +10,13 @@ echo "Você só pode entrar se tiver a partir de 18 anos ou";
  echo " a partir de 16 anos se tiver acompanhado." . PHP_EOL;
 if($idade >= 18){
     echo "Você tem $idade anos . Pode entrar sozinho. " . PHP_EOL;
-    
 }else if($idade >= 16 && $numeroDePessoas > 1){
     echo "Você tem $idade anos , está acompanhado(a) , então pode entrar.";
-
 }else{
     echo "Você só tem $idade anos . Você não pode entrar";
 }
+    
+
 
 
 echo PHP_EOL;
